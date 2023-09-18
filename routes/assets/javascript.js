c7227dd5a5b1380b1c3b1bdedaee88566926bad9
Benjamin Marsh <sparty182020@gmail.com>
2023-09-18 09:45:55 -0400

    update the routes

--- routes/assets/javascript.js
+++ routes/assets/javascript.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { aprilFools } = require('../functions/utilities');
+const { aprilFools } = require('../../functions/utilities');
 
 router
     .get(`/fs/:file`, (req, res) => {
--- routes/assets/router.js
+++ routes/assets/router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { aprilFools } = require('../functions/utilities');
+const { aprilFools } = require('../../functions/utilities');
 
 const css = require('./css');
 const images = require('./images');
