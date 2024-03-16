d417fc10ed8498addd6fcd569a2c083252fb0da1
Benjamin <therealbenpai@gmail.com>
2024-03-16 17:11:52 -0400

    updates to CSP and CORS

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,21 +1,41 @@
-const CORS = (data) => {
-    let corsStr = '';
-    for (const [key, value] of Object.entries(data)) {
-        corsStr += `${key.replace(/([A-Z])/g, '-$1').toLowerCase()}`;
-        const fValue = Object.assign({}, { wildcard: false, domains: [], inititives: [], self: false, none: false }, value);
-        if (fValue.none) {
+class WebSecurity {
+    static CSP(data) {
+        let corsStr = '';
+        for (const [key, value] of Object.entries(data)) {
+            corsStr += `${key.replace(/([A-Z])/g, '-$1').toLowerCase()}`;
+            const fValue = Object.assign({}, { wildcard: false, domains: [], inititives: [], self: false, none: false }, value);
+            if (fValue.none) {
+                corsStr += `'none'; `;
+                continue;
+            }
+            if (fValue.inititives.length > 0) corsStr += ` ${fValue.inititives.join(' ')}`;
+            if (fValue.self) corsStr += ` 'self'`;
+            if (fValue.wildcard) corsStr += ` *`;
+            if (fValue.domains.length > 0) corsStr += ` ${fValue.domains.join(' ')}`;
             corsStr += `; `;
-            continue;
         }
-        if (fValue.wildcard) corsStr += ` *`;
-        if (fValue.domains.length > 0) corsStr += ` ${fValue.domains.join(' ')}`;
-        if (fValue.inititives.length > 0) corsStr += ` ${fValue.inititives.join(' ')}`;
-        if (fValue.self) corsStr += ` 'self'`;
-        corsStr += `; `;
+        return corsStr.slice(0, -1);
     }
-    return corsStr.slice(0, -1);
+    static CORS(data) {
+        const CORSPolicies = {
+            allowOrigin: { wildcard: false, domain: '' },
+            exposeHeaders: { wildcard: false, headers: [] },
+            maxAge: 0,
+            allowCredentials: false,
+            allowMethods: [],
+            allowHeaders: []
+        }
+        const fData = Object.assign({}, CORSPolicies, data);
+        const Headers = new Set();
+        Headers.add(['Access-Control-Allow-Origin', (fData.allowOrigin.domain) ? fData.allowOrigin.domain : '*']);
+        Headers.add(['Access-Control-Expose-Headers', (fData.exposeHeaders.headers.length > 0) ? fData.exposeHeaders.headers.join(', ') : '*']);
+        Headers.add(['Access-Control-Max-Age', fData.maxAge]);
+        Headers.add(['Access-Control-Allow-Credentials', fData.allowCredentials]);
+        Headers.add(['Access-Control-Allow-Methods', (fData.allowMethods.length > 0) ? fData.allowMethods.join(', ') : '*']);
+        Headers.add(['Access-Control-Allow-Headers', (fData.allowHeaders.length > 0) ? fData.allowHeaders.join(', ') : '*']);
+        return Array.from(Headers);
+    }
 }
-
 
 /** @type {import('express').RequestHandler} */ module.exports = (req, res, next) => {
     const { platform: os, versions: v } = process;
@@ -25,13 +45,13 @@ const CORS = (data) => {
         .setHeader('X-Repository-License', 'Affero General Public License v3.0 or newer (AGPL-3.0-or-later)')
         .setHeader('X-OS', os == 'win32' ? 'Windows' : os == 'linux' ? 'Linux' : os == 'darwin' ? 'MacOS' : 'Other')
         .setHeader('X-Node-Version', v.node)
-        .setHeader('Content-Security-Policy', CORS({
-            defaultSrc: {wildcard: true},
+        .setHeader('Content-Security-Policy', WebSecurity.CSP({
+            defaultSrc: { wildcard: true },
             scriptSrc: {
                 self: true,
                 domains: [
-                    'google.com',
                     '*.google.com',
+                    'google.com',
                     '*.googlesyndication.com',
                     'googlesyndication.com',
                     '*.googleadservices.com',
@@ -41,8 +61,9 @@ const CORS = (data) => {
                     '*.sentry-cdn.com',
                     'sentry-cdn.com',
                     '*.thefemdevs.com',
-                    'fontawesome.com',
+                    'thefemdevs.com',
                     '*.fontawesome.com',
+                    'fontawesome.com',
                     'blob:'
                 ],
                 inititives: ['unsafe-inline', 'unsafe-eval']
@@ -50,27 +71,37 @@ const CORS = (data) => {
             styleSrc: {
                 self: true,
                 domains: [
+                    "*.google.com",
+                    "google.com",
+                    "*.googleapis.com",
                     'fonts.googleapis.com',
-                    'cdn.thefemdevs.com',
-                    'fontawesome.com',
+                    '*.thefemdevs.com',
+                    'thefemdevs.com',
                     '*.fontawesome.com',
+                    'fontawesome.com',
                 ],
                 inititives: ['unsafe-inline', 'unsafe-eval']
             },
-            imgSrc: {wildcard: true},
-            fontSrc: {wildcard: true},
-            connectSrc: {wildcard: true},
-            mediaSrc: {wildcard: true},
-            objectSrc: {none: true},
-            frameSrc: {wildcard: true},
-            formAction: {self: true},
-            baseUri: {self: true},
-            manifestSrc: {self: true},
-            requireTrustedTypesFor: {inititives: ['script']}
+            imgSrc: { wildcard: true },
+            fontSrc: { wildcard: true },
+            connectSrc: { wildcard: true },
+            mediaSrc: { wildcard: true },
+            objectSrc: { none: true },
+            frameSrc: { wildcard: true },
+            formAction: { self: true },
+            baseUri: { self: true },
+            manifestSrc: { self: true },
+            requireTrustedTypesFor: { inititives: ['script'] }
         }))
         .setHeader('Cross-Origin-Opener-Policy', 'same-origin')
         .setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
         .setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
         .setHeader("Document-Policy", "js-profiling")
+    WebSecurity.CORS({
+        maxAge: 86400,
+        allowCredentials: true,
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-HTTP-Method-Override', 'Accept', 'Origin']
+    }).forEach(([key, value]) => res.setHeader(key, value));
     return next();
 }
