3fa1f31b8080948ed15c64268a6ee42694027369
Benjamin <therealbenpai@gmail.com>
2024-03-16 16:42:34 -0400

    updates to cdns

--- web/pages/routes/alex.js
+++ web/pages/routes/alex.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 router
     .get('/', (_, res) => res.render('pages/alex/index.pug'))
-    .get('/cdn', require('../functions/routeCDN')('alex'));
+    .get('/cdn/img/:file', (req, res) => res.sendFile(`${process.cwd()}/web/pages/assets/alex/img/${req.params.file}`))
+    .get('/cdn/js/:file', (req, res) => res.sendFile(`${process.cwd()}/web/pages/assets/alex/js/${req.params.file}.js`))
+    .get('/cdn/css/:file', (req, res) => res.sendFile(`${process.cwd()}/web/pages/assets/alex/css/${req.params.file}.css`));
 
 module.exports = router;
--- web/pages/routes/ben.js
+++ web/pages/routes/ben.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 router
     .get('/', (_, res) => res.render('pages/ben/index.pug'))
-    .get('/cdn', require('../functions/routeCDN')('ben'));
+    .get('/cdn/img/:file', (req, res) => res.sendFile(`${process.cwd()}/web/pages/assets/ben/img/${req.params.file}`))
+    .get('/cdn/js/:file', (req, res) => res.sendFile(`${process.cwd()}/web/pages/assets/ben/js/${req.params.file}.js`))
+    .get('/cdn/css/:file', (req, res) => res.sendFile(`${process.cwd()}/web/pages/assets/ben/css/${req.params.file}.css`));
 
 module.exports = router;
