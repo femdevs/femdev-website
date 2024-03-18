f80ef12ab4f8ce1d3dfda1c465ac7c762e0c0133
Benjamin <therealbenpai@gmail.com>
2024-03-17 20:22:14 -0400

    more CSP code

--- web/security/routes/csp.js
+++ web/security/routes/csp.js
@@ -3,14 +3,15 @@ const router = require('express').Router();
 const CSPData = require('../../../functions/security');
 
 router
-    .use(require('express').json({ type: ['application/csp-report', 'application/reports+json'] }))
+    .use(require('express').json({ type: ['application/csp-report', 'application/reports+json'], inflate: true, strict: false, limit: '10mb' }))
     .post('/new', (req, res) => {
+        console.log(req.headers['content-type'])
+        console.log(req.body)
         switch (req.headers['content-type']) {
             case 'application/csp-report':
                 // Firefox
-                console.dir(req.body)
-                // const data = new CSPData(req.body);
-                // req.Database.SaveCSPReport(data);
+                const data = new CSPData(req.body['csp-report']);
+                req.Database.SaveCSPReport(data);
                 res.status(204).end();
                 break;
             case 'application/reports+json':
