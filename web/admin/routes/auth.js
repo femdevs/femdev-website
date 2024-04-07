f1fbd159ff67ff7b709462b5ea9619050c2cfa80
Benjamin <therealbenpai@gmail.com>
2024-04-07 10:27:57 -0400

    updates to APIs

--- web/admin/routes/auth.js
+++ web/admin/routes/auth.js
@@ -3,19 +3,11 @@ const router = require('express').Router();
 router
     .get('/login', (req, res) => res.render('auth/login'))
     .post('/login', (req, res) => {
+        const { username, password } = req.body;
         // Login logic here
     })
     .get('/logout', (req, res) => {
         // Logout logic here
-    })
-    .get('/verify', (req, res) => {
-        // Verify email logic here
-    })
-    .get('/verify/:token', (req, res) => {
-        // Verify token logic here
-    })
-    .post('/verify', (req, res) => {
-
     })
 
 module.exports = router;
