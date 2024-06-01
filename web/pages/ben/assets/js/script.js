f585b8dbee2a00f740208c91dd710e5aa7764c28
Benjamin <benpai@thefemdevs.com>
2024-06-01 08:46:09 -0400

    get the needed data from the spotify api

--- web/pages/ben/assets/js/script.js
+++ web/pages/ben/assets/js/script.js
@@ -5,54 +5,59 @@ Elements
     .set('spotify', document.getElementById('spotify'))
     .set('discord', document.getElementById('discord'));
 
+class Discord {
+    constructor() {
+        this.status = 'offline';
+        this.activities = [];
+    }
+    getString() {
+        return `${this.status} - ${this.activities[0]?.state || 'No Activities'}`;
+    }
+}
+
+class Spotify {
+    constructor() {
+        this.playing = false;
+        this.track = {
+            name: 'Nothing',
+            album: 'Nothing',
+            artists: [],
+            url: 'https://open.spotify.com',
+        };
+    }
+    getString() {
+        return this.playing
+            ? `${this.track.name} by ${this.track.artists.slice(0, 2).join(', ')}`
+            : 'Not Playing';
+    }
+}
+
 const load = async () => {
     const req = await fetch('https://api.lanyard.rest/v1/users/1158220643616182445');
-    const importantData = {
-        discord: {
-            status: 'offline',
-            activities: [],
-        },
-        spotify: {
-            playing: false,
-            track: {
-                name: 'Nothing',
-                album: 'Nothing',
-                artists: [],
-                url: 'https://open.spotify.com',
-            },
-        },
-    };
     if (req.status === 200) {
+        const discord = new Discord();
+        const spotify = new Spotify();
         const { data } = await req.json();
-        importantData.discord.status = data.discord_status === 'online' ? 'Online'
-            : data.discord_status === 'dnd' ? 'Do Not Disturb'
-                : data.discord_status === 'idle' ? 'Idle'
-                    : 'Offline';
-        importantData.discord.activities = data.activities
-            .map(({ state, id }) => ({
-                state,
-                id,
-            }))
-            .filter(item => item.id !== 'spotify:1');
-        if (data.spotify) {
-            Object.assign(importantData.spotify, {
-                playing: data.listening_to_spotify,
-                track: {
-                    name: data.spotify.song,
-                    album: data.spotify.album,
-                    artists: data.spotify.artist.split(';').map(art => art.trim()),
-                    url: `https://open.spotify.com/track/${data.spotify.track_id}`,
-                },
-            });
+        switch (data.discord_status) {
+            case 'online':
+                discord.status = 'Online';
+                break;
+            case 'dnd':
+                discord.status = 'Do Not Disturb';
+                break;
+            case 'idle':
+                discord.status = 'Idle';
+                break;
+            default:
+                discord.status = 'Offline';
+                break;
         }
-        let discordString = `${importantData.discord.status} `;
-        if (importantData.discord.activities.length > 0)
-            discordString += "- " + importantData.discord.activities[0].state;
-        Elements.get('discord').innerText = discordString;
-        let spotifyString = `${importantData.spotify.playing ? 'Playing' : 'Not Playing'} `;
-        if (importantData.spotify.playing)
-            spotifyString += `${importantData.spotify.track.name} by ${importantData.spotify.track.artists.slice(0, 2).join(', ')}`;
-        Elements.get('spotify').innerText = spotifyString;
+        discord.activities = data.activities.filter(item => item.id !== 'spotify:1');
+        const spotifyData = await fetch('https://ben.thefemdevs.com/api/now-playing').then(res => res.json());
+        spotify.playing = spotifyData.isPlaying;
+        spotify.track = spotifyData.song ?? {};
+        Elements.get('discord').innerText = discord.getString();
+        Elements.get('spotify').innerText = spotify.getString();
     };
 };
 
---
+++ web/pages/ben/routes/api.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const SpotifyWebApi = require("spotify-web-api-node");
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+});
+spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
+spotifyApi.setRefreshToken(process.env.SPOTIFY_REFRESH_TOKEN);
+setInterval(() => spotifyApi.refreshAccessToken().then(({ body: { access_token } }) => spotifyApi.setAccessToken(access_token)), 3e5);
+router
+    .get("/now-playing", async (req, res) => {
+        res.json(await spotifyApi.getMyCurrentPlaybackState({}).then(data => {
+            const { body } = data;
+            if (!new Object(body).hasOwnProperty('item')) return { isPlaying: false };
+            const { name, album, artists, external_urls } = body.item;
+            const song = {
+                name,
+                album: album.name,
+                artists: artists.map(artist => artist.name),
+                url: external_urls.spotify,
+            };
+            return {
+                isPlaying: true,
+                song,
+            };
+        }));
+    })
+    .use((req, res, next) => {
+        const { path } = req;
+        const methodUsed = req.method.toUpperCase();
+        let allowedMethods = router.stack.filter(routerObj => routerObj.route && routerObj.route.path === path);
+        if (allowedMethods.length === 0) return next();
+        allowedMethods.map(routerObj => routerObj.route.stack[0]);
+        allowedMethods = { ...allowedMethods[0] };
+        allowedMethods = allowedMethods.route.methods;
+        if (req.method === 'OPTIONS')
+            return res.setHeader('Allow', Object.keys(allowedMethods)
+                .map(verb => verb.toUpperCase()).join(', '))
+                .setHeader('Access-Control-Allow-Methods', Object.keys(allowedMethods).map(verb => verb.toUpperCase()).join(', '))
+                .status(204)
+                .send();
+        if (allowedMethods[methodUsed]) return next();
+        return res.status(405).render(
+            "misc/405.pug",
+            req.getErrPage(405, { path, allowedMethods, methodUsed }),
+        );
+    });
+
+module.exports = router;
