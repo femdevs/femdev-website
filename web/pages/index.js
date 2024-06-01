a7c2735f6c163a28d92d8242f3b7fffb3c48c3dc
Benjamin <benpai@thefemdevs.com>
2024-05-31 20:33:33 -0400

    give nezha a website

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const vhost = require('vhost');
 
-const alex = require('./alex/');
 const ben = require('./ben/');
+const alex = require('./alex/');
+const nezha = require('./nezha/');
 const oblong = require('./oblong/');
 
 router
-	.use(vhost('alex.thefemdevs.com', alex))
 	.use(vhost('ben.thefemdevs.com', ben))
+	.use(vhost('alex.thefemdevs.com', alex))
+	.use(vhost('nezha.thefemdevs.com', nezha))
 	.use(vhost('oblong.thefemdevs.com', oblong));
 
 module.exports = router;
