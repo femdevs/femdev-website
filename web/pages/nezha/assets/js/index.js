77083d23b1a491d88e01bf0b150511c98f4e8298
Benjamin <benpai@thefemdevs.com>
2024-06-28 17:17:12 -0400

    fix nezha's page

--- web/pages/nezha/assets/js/index.js
+++ web/pages/nezha/assets/js/index.js
@@ -1,26 +1,24 @@
 import { LoadData as load } from "https://cdn.thefemdevs.com/assets/js/o/spotify";
-const { random, floor } = Math;
 
 document.addEventListener(
     'DOMContentLoaded', // Only triggers once the page is fully loaded - prevents errors
     async () => {
         await load('nezha', '1109887155720945664');
         setInterval(async () => await load('nezha', '1109887155720945664'), 1e3);
-        const charCodes = {
-            start: 32, // Start: 32 (space)
-            end: 126, // End: 126 (tilde)
-            rand() { return floor(random() * (this.start - this.end + 1)) + this.start; },
+        const getRandChar = () => {
+            const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?~ ';
+            return chars[Math.floor(Math.random() * chars.length)];
         };
         setInterval((function () {
             const elements = Array.from(document.querySelectorAll('span.ScrollingASCII'));
             for (const element of elements) {
                 let newText = '', index = 0;
                 while (index < element.textContent.length) {
-                    newText += String.fromCharCode(charCodes.rand());
+                    newText += String.fromCharCode(getRandChar());
                     index++;
                 }
                 element.textContent = newText;
             }
-        }), 10);
+        }), 50);
     },
 );
