1546c9f67725dc9d6abd35273b42270c2b512080
Benjamin <benpai@thefemdevs.com>
2024-08-11 12:10:10 -0400

    fix player again

--- web/pages/ben/assets/js/player.js
+++ web/pages/ben/assets/js/player.js
@@ -38,15 +38,14 @@ class Spotify {
 }
 
 async function fetchNowPlaying() {
-    try {
-        const response = await fetch('https://spotify.thefemdevs.com/playing/ben');
-        const data = await response.json();
+    const response = await fetch('https://spotify.thefemdevs.com/playing/ben');
+    const data = await response.json();
 
-        const spotify = new Spotify();
-        spotify.setPlaying(data.isPlaying);
-        spotify.setTrack(spotify.playing);
+    const spotify = new Spotify();
+    spotify.setPlaying(data.isPlaying);
+    spotify.setTrack(spotify.playing);
 
-        const AlbumCSS = `
+    const AlbumCSS = `
 .album-cover::before {
     background-image: url('https://via.placeholder.com/300');
     content: '';
@@ -61,27 +60,25 @@ async function fetchNowPlaying() {
     z-index: 0;
 }`;
 
-        if (spotify.playing) {
-            const strings = spotify.getString();
-            document.getElementById('album-cover').src = spotify.song.album.image;
-            document.getElementById('bg').innerHTML = AlbumCSS.replace(/(url\(.*\))/gmi, `url('${spotify.song.album.image}')`);
-            document.getElementById('song-title').textContent = strings[0];
-            document.getElementById('artist-name').textContent = strings[1];
-            const pad = num => num.toString().padStart(2, '0');
-            const currentTime = Math.floor(spotify.song.meta.progress.current / 1000);
-            const duration = Math.floor((spotify.song.meta.progress.end - spotify.song.meta.progress.start) / 1000);
-            document.getElementById('progress-bar').style.width = `${spotify.song.meta.progress.percentage}%`;
-            document.getElementById('current-time').textContent = `${Math.floor(currentTime / 60)}:${pad(currentTime % 60)}`;
-            document.getElementById('duration-time').textContent = `${Math.floor(duration / 60)}:${pad(duration % 60)}`;
-        } else {
-            document.getElementById('album-cover').src = 'https://via.placeholder.com/300';
-            document.getElementById('bg').innerHTML = AlbumCSS;
-            document.getElementById('song-title').textContent = 'Nothing';
-            document.getElementById('artist-name').textContent = 'No One';
-            document.getElementById('current-time').textContent = `--:--`;
-            document.getElementById('duration-time').textContent = '--:--';
-        }
-    } catch (error) {
+    if (spotify.playing) {
+        const strings = spotify.getString();
+        document.getElementById('album-cover').src = spotify.song.album.image;
+        document.getElementById('bg').innerHTML = AlbumCSS.replace(/(url\(.*\))/gmi, `url('${spotify.song.album.image}')`);
+        document.getElementById('song-title').textContent = strings[0];
+        document.getElementById('artist-name').textContent = strings[1];
+        const pad = num => num.toString().padStart(2, '0');
+        const currentTime = Math.floor(spotify.song.meta.progress.current / 1000);
+        const duration = Math.floor((spotify.song.meta.progress.end - spotify.song.meta.progress.start) / 1000);
+        document.getElementById('progress-bar').style.width = `${spotify.song.meta.progress.percentage}%`;
+        document.getElementById('current-time').textContent = `${pad(Math.floor(currentTime / 60))}:${pad(currentTime % 60)}`;
+        document.getElementById('duration-time').textContent = `${pad(Math.floor(duration / 60))}:${pad(duration % 60)}`;
+    } else {
+        document.getElementById('album-cover').src = 'https://via.placeholder.com/300';
+        document.getElementById('bg').innerHTML = AlbumCSS;
+        document.getElementById('song-title').textContent = 'Nothing';
+        document.getElementById('artist-name').textContent = 'No One';
+        document.getElementById('current-time').textContent = `--:--`;
+        document.getElementById('duration-time').textContent = '--:--';
     }
 }
 
